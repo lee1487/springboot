0d3d54908b805b1e8d08c0b1fa8e9471137e6247
hyeonse <dlgustp1487@naver.com>
2022-09-30 19:40:09 +0900

    내장 웹 서버 응용 1부 : 컨테이너와 포트

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -1,45 +1,13 @@
 package com.springboot;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-
+@SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) throws LifecycleException, IOException {
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8080);
-		String docBase = Files.createTempDirectory("tomcat-basedir").toString();
-		Context context = tomcat.addContext("/", docBase);
-
-		HttpServlet servlet = new HttpServlet() {
-			@Override
-			protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-					throws ServletException, IOException {
-				PrintWriter writer = resp.getWriter();
-				writer.println("<html><head><title>");
-				writer.println("Hey, Tomcat");
-				writer.println("</title></head>");
-				writer.println("<body><h1>Hello Tomcat</h1></body>");
-				writer.println("</html>");
-			}
-		};
-
-		String servletName = "helloServlet";
-		tomcat.addServlet("/", servletName, servlet);
-		context.addServletMappingDecoded("/hello", servletName);
-		tomcat.getConnector();
-		tomcat.start();
-
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
 }
