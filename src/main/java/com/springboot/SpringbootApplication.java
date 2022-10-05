11a1de3d671b78a1560b9bcdbc49a646b09c16b9
hyeonse <dlgustp1487@naver.com>
2022-10-05 20:22:43 +0900

    SpringApplication 1부

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -2,31 +2,18 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootApplication {
 
-	@GetMapping("/")
-	public String hello() {
-		return "HELLO SPRING";
-	}
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringbootApplication.class);
+		app.setBanner((environment, sourceClass, out) -> {
+			out.println("==================");
+			out.println("HyeonSe");
+			out.println("==================");
+		});
+		//app.setBannerMode(Banner.Mode.OFF);
+ 		app.run(args);
 	}
-
-//	@Bean
-//	public ServletWebServerFactory serverFactory() {
-//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-//		return tomcat;
-//	}
-//
-//	private Connector createStandardConnector() {
-//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//		connector.setPort(8080);
-//		return connector;
-//	}
 }
