3f3a12a8d31a8e434992e8ce6011119fcc8baf41
hyeonse <dlgustp1487@naver.com>
2022-10-07 23:30:09 +0900

    외부 설정 1부 진행중

--- src/main/java/com/springboot/SampleListener.java
+++ src/main/java/com/springboot/SampleListener.java
@@ -1,21 +1,22 @@
 package com.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 public class SampleListener implements ApplicationRunner{
 
-//	public SampleListener(ApplicationArguments arguments) {
-//		System.out.println("foo: " + arguments.containsOption("foo"));
-//		System.out.println("bar: " + arguments.containsOption("bar"));
-//	}
+	@Value("${hyeonse.name}")
+	private String name;
 
 	@Override
 	public void run(ApplicationArguments arguments) throws Exception {
-		System.out.println("foo: " + arguments.containsOption("foo"));
-		System.out.println("bar: " + arguments.containsOption("bar"));
+		System.out.println("================");
+		System.out.println(name);
+		System.out.println("================");
 	}
 
 
