9b15b12f0e8471e944d8917aca599e94e995ca55
hyeonse <dlgustp1487@naver.com>
2022-10-14 09:14:23 +0900

    테스트 진행중

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -1,21 +1,13 @@
 package com.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-//	@Bean
-//	@ConfigurationProperties("server")
-//	public ServerProperties serverProperties() {
-//		return new ServerProperties();
-//	}
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SpringbootApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
- 		app.run(args);
+		SpringApplication.run(SpringbootApplication.class, args);
 	}
 }
