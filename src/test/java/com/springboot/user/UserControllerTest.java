fcec0c16883136abb4c27c598592afbbafdb576e
hyeonse <dlgustp1487@naver.com>
2022-10-19 20:53:23 +0900

    스프링 웹 MVC 1부: 소개

---
+++ src/test/java/com/springboot/user/UserControllerTest.java
@@ -0,0 +1,26 @@
+package com.springboot.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void hello() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("hello"));
+	}
+}
