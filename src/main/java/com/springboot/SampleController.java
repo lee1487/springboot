a057dcf239368a3ea0edc0689d5f00343efb0859
hyeonse <dlgustp1487@naver.com>
2022-10-27 20:08:36 +0900

    스프링 웹 MVC 9부: ExceptionHandler

--- src/main/java/com/springboot/SampleController.java
+++ src/main/java/com/springboot/SampleController.java
@@ -1,8 +1,11 @@
 package com.springboot;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
@@ -12,4 +15,17 @@ public class SampleController {
 		model.addAttribute("name", "hyeonse");
 		return "hello";
 	}
+
+	@GetMapping("/exception")
+	public String exception() {
+		throw new SampleException();
+	}
+
+	/*
+	 * @ExceptionHandler(SampleException.class)
+	 *
+	 * public @ResponseBody AppError sampleError(SampleException e) { AppError
+	 * appError = new AppError(); appError.setMessage("error.app.key");
+	 * appError.setReason("IDK IDK IDK"); return appError; }
+	 */
 }
