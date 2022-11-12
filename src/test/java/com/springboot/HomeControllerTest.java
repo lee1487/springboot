018bd1587beea080ad593b5f0b77d1a13aa980d0
hyeonse <dlgustp1487@naver.com>
2022-11-12 13:37:20 +0900

    스프링 시큐리티 1부: Starter-Security

--- src/test/java/com/springboot/HomeControllerTest.java
+++ src/test/java/com/springboot/HomeControllerTest.java
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -16,10 +18,20 @@ class HomeControllerTest {
 	@Autowired MockMvc mockMvc;
 
 	@Test
+	@WithMockUser
 	public void hello() throws Exception {
-		mockMvc.perform(get("/hello"))
+		mockMvc.perform(get("/hello")
+					.accept(MediaType.TEXT_HTML))
 				.andExpect(status().isOk())
 				.andExpect(view().name("hello"));
+	}
+
+	@Test
+	@WithMockUser
+	public void my() throws Exception {
+		mockMvc.perform(get("/my"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("my"));
 	}
 
 }
