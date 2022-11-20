5f9d8b3e675d9e7a38d5a83b968a6c6508a01a2c
hyeonse <dlgustp1487@naver.com>
2022-11-20 11:40:35 +0900

    스프링부트 2.1 - 로거 변경하기

--- src/test/java/com/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/springboot/SpringbootApplicationTests.java
@@ -1,11 +1,11 @@
 package com.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
 @SpringBootTest
 class SpringbootApplicationTests {
