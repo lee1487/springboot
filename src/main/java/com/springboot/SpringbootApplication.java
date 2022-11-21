1de0f2e6b302240f831e1f53fe27a50c7545b168
hyeonse <dlgustp1487@naver.com>
2022-11-21 13:40:29 +0900

    빈 오버라이딩 기본 설정 변경

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -1,5 +1,8 @@
 package com.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
@@ -13,5 +16,26 @@ public class SpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
+	
+	@Bean
+	public ApplicationRunner applicationRunner () {
+		return new ApplicationRunner() {
+			
+			@Autowired FamilyMan familyMan;
+			
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+				System.out.println("==============================");
+				System.out.println(familyMan.getName());
+				System.out.println("==============================");
+			}
+		};
+	}
 
+	@Bean
+	public FamilyMan familyMan() {
+		FamilyMan familyMan = new FamilyMan();
+		familyMan.setName("hyeonse");
+		return familyMan;
+	}
 }
