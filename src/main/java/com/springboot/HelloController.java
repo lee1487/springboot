438d2d6c1aebe4cbdb972e5758a13355e9889a49
hyeonse <dlgustp1487@naver.com>
2022-11-25 13:04:49 +0900

    DataSize

--- src/main/java/com/springboot/HelloController.java
+++ src/main/java/com/springboot/HelloController.java
@@ -1,13 +1,22 @@
 package com.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@Value("${chickenSize}")
+	private DataSize chickenSize;
 
 	@GetMapping("/hello")
 	public String hello() {
+		System.out.println("============");
+		System.out.println(chickenSize);
+		System.out.println(chickenSize.toMegabytes());
+		System.out.println("============");
 		return "hello";
 	}
 }
--- src/test/java/com/springboot/HelloControllerTest.java
+++ src/test/java/com/springboot/HelloControllerTest.java
@@ -4,11 +4,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,9 +20,12 @@ class HelloControllerTest {
 
 	@Test
 	public void testHello() throws Exception{
-		mockMvc.perform(get("/hello"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("hello"));
+		ResultActions resultActions = mockMvc.perform(get("/hello"));
+		
+		Assertions.assertAll(
+				() -> resultActions.andExpect(status().isOk()),
+				() -> resultActions.andExpect(content().string("hello")));
+		
 	}
 
 }
