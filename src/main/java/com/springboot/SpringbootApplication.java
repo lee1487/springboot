5efceaf8d18c556ab6ca18d224b23e7730005179
hyeonse <dlgustp1487@naver.com>
2022-12-05 17:54:08 +0900

    의존성 및 프로퍼티 변경

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -1,33 +1,26 @@
 package com.springboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
+import java.time.Period;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class SpringbootApplication {
+
+	@Value("${chicken.howlong}")
+	private Period chickenHowLong;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-	
+
 	@Bean
 	public ApplicationRunner applicationRunner() {
-		return new ApplicationRunner() {
-			
-			@Autowired ChickenProperties chickenProperties;
-			
-			@Override
-			public void run(ApplicationArguments args) throws Exception {
-				System.out.println(chickenProperties.getName());
-				System.out.println(chickenProperties.getCount());
-				System.out.println(chickenProperties.getSize());
-			}
-		};
+		return args -> System.out.println(chickenHowLong.getMonths());
 	}
+
 }
